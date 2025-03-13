e963ad8f661cdb55845e092c3fcb8539378be043
desarrollocolmayor <desarrollocolmayor@colmayor.edu.co>
2025-03-13 18:24:36 -0500

    Se arregla Home con dimensiones anteriores

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,24 +9,61 @@ import ROICalculator from "../components/ROICalculator";
 import PressSlider from "../components/PressSlider";
 import LiveDemoForm from "../components/LiveDemoForm";
 
+const Layout = ({ children }) => {
+    return (
+        <div className="relative w-full max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+            {children}
+        </div>
+    );
+};
+
 const Home = () => {
     return (
         <>
-            <div className="relative flex flex-col items-center">
-                <Hero />
-            </div>
+            {/* Sección Hero */}
+            <Layout>
+                <div className="relative flex flex-col items-center">
+                    <Hero />
+                </div>
+            </Layout>
+
+            {/* Dashboard flotando sobre Hero */}
             <div className="relative flex justify-center -mt-10 sm:-mt-16 z-20 bg-[#FAFAFA]">
-                <Dashboard />
+                <Layout>
+                    <Dashboard />
+                </Layout>
             </div>
-            <div className="bg-[#FAFAFA]"><Slider /></div>
-            <div className="bg-[#FAFAFA]"><DashboardContainer /></div>
-            <div className="bg-[#FAFAFA]"><DashboardContainerLow /></div>
-            <div className="bg-[#FAFAFA]"><Banner /></div>
-            <div className="bg-[#FAFAFA]"><CaseStudies /></div>
-            <div className="bg-[#FAFAFA]"><ROICalculator /></div>
-            <div className="bg-[#FAFAFA]"><PressSlider /></div>
+
+            {/* Secciones con fondo blanco */}
+            <div className="bg-[#FAFAFA]">
+                <Layout><Slider /></Layout>
+            </div>
+            <div className="bg-[#FAFAFA]">
+                <Layout><DashboardContainer /></Layout>
+            </div>
+            <div className="bg-[#FAFAFA]">
+                <Layout><DashboardContainerLow /></Layout>
+            </div>
+            <div className="bg-[#FAFAFA]">
+                <Layout><Banner /></Layout>
+            </div>
+            <div className="bg-[#FAFAFA]">
+                <Layout><CaseStudies /></Layout>
+            </div>
+            <div className="bg-[#FAFAFA]">
+                <Layout><ROICalculator /></Layout>
+            </div>
+            <div className="bg-[#FAFAFA]">
+                <Layout><PressSlider /></Layout>
+            </div>
+
+            {/* Sección con fondo negro */}
             <div className="bg-black py-20">
-                <LiveDemoForm />
+                <Layout>
+                    <div className="max-w-7xl mx-auto">
+                        <LiveDemoForm />
+                    </div>
+                </Layout>
             </div>
         </>
     );
